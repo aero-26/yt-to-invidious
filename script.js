fd3edeab69278467ae976d26ca2415a08040baab
aero-26 <shubhankarbanerjee16@gmail.com>
2024-03-06 10:32:59 +0530

    Finalised the Project

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const inviTextArea = document.getElementById("invidious");
+const ytTextArea = document.getElementById("yt");
+const btn = document.getElementsByClassName("btn")[0];
+const converted = document.getElementById("converted");
+const goBtn = document.getElementById("go");
+
+const convert = function () {
+  let a = inviTextArea.value;
+  let b = ytTextArea.value;
+  if (a != "" && b != "") {
+    let replace = b.replace("www.youtube.com", a);
+    converted.value = replace;
+    goBtn.classList.remove("hidden");
+  }
+};
+
+btn.addEventListener("click", () => convert());
+
+goBtn.addEventListener("click", () => window.open(converted.value));
